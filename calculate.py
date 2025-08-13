9f299790f88df2f6fc5f7f5d4db5505d05d5ec67
Gaurund <pavel.makushev@gmail.com>
2025-08-13 17:16:37 +0300

    MyCalc works

--- calculate.py
+++ calculate.py
@@ -4,29 +4,63 @@ from tkinter import ttk
 root = Tk()
 root.title("Simple Calculator")
 
+numbers = []
+operations = []
+
 
 def button_click(number):
     # entry.delete(0, "end")
     current = entry.get()
     entry.insert("end", number)
 
+
 def add():
-    first_number = entry.get()
-    global f_num
-    f_num = int(first_number)
-    clear_all()
-
-def equal():
-    second_number = entry.get()
-    clear_all()
-    entry.insert(0, str(f_num + int(second_number)))
-
-def clear_all():
+    numbers.append(entry.get())
+    operations.append("+")
     entry.delete(0, "end")
 
 
+def subtract():
+    numbers.append(entry.get())
+    operations.append("-")
+    entry.delete(0, "end")
+
+
+def multiply():
+    numbers.append(entry.get())
+    operations.append("*")
+    entry.delete(0, "end")
+
+
+def divide():
+    numbers.append(entry.get())
+    operations.append("/")
+    entry.delete(0, "end")
+
+
+def equal():
+    operations.append(" ")
+    last_number = entry.get()
+    if last_number == "":
+        last_number = 0
+    numbers.append(last_number)
+    result = ""
+    for i in range(len(numbers)):
+        result += f"{numbers[i]}{operations[i]}"
+    numbers.clear()
+    operations.clear()
+    entry.delete(0, "end")    
+    entry.insert(0, eval(result))
+
+
+def clear_all():
+    entry.delete(0, "end")
+    numbers.clear()
+    operations.clear()
+
+
 entry = ttk.Entry(root, width=35)
-entry.grid(column=0, row=0, columnspan=3, padx=10, pady=10)
+entry.grid(column=0, row=0, columnspan=3, padx=10, pady=10, sticky="nsew")
 
 button_1 = ttk.Button(root, text="1", command=lambda: button_click(1))
 button_2 = ttk.Button(root, text="2", command=lambda: button_click(2))
@@ -41,25 +75,28 @@ button_0 = ttk.Button(root, text="0", command=lambda: button_click(0))
 button_add = ttk.Button(root, text="+", command=add)
 button_equal = ttk.Button(root, text="=", command=equal)
 button_clear = ttk.Button(root, text="Clear", command=clear_all)
-button_subtruct = ttk.Button(root, text="-", command=add)
-button_equal = ttk.Button(root, text="=", command=equal)
-button_clear = ttk.Button(root, text="Clear", command=clear_all)
-
-button_1.grid(row=3, column=0, sticky="nsew")
-button_2.grid(row=3, column=1, sticky="nsew")
-button_3.grid(row=3, column=2, sticky="nsew")
-
-button_4.grid(row=2, column=0, sticky="nsew")
-button_5.grid(row=2, column=1, sticky="nsew")
-button_6.grid(row=2, column=2, sticky="nsew")
+button_substract = ttk.Button(root, text="-", command=subtract)
+button_multiply = ttk.Button(root, text="*", command=multiply)
+button_divide = ttk.Button(root, text="/", command=divide)
 
 button_7.grid(row=1, column=0, sticky="nsew")
 button_8.grid(row=1, column=1, sticky="nsew")
 button_9.grid(row=1, column=2, sticky="nsew")
+button_add.grid(row=1, column=3, sticky="nsew")
 
-button_0.grid(row=4, column=0, sticky="nsew")
-button_clear.grid(row=4, column=1, columnspan=2, sticky="nsew")
-button_add.grid(row=5, column=0, sticky="nsew")
-button_equal.grid(row=5, column=1, columnspan=2, sticky="nsew")
+button_4.grid(row=2, column=0, sticky="nsew")
+button_5.grid(row=2, column=1, sticky="nsew")
+button_6.grid(row=2, column=2, sticky="nsew")
+button_substract.grid(row=2, column=3, sticky="nsew")
+
+button_1.grid(row=3, column=0, sticky="nsew")
+button_2.grid(row=3, column=1, sticky="nsew")
+button_3.grid(row=3, column=2, sticky="nsew")
+button_multiply.grid(row=3, column=3, sticky="nsew")
+
+button_clear.grid(row=4, column=0, sticky="nsew")
+button_0.grid(row=4, column=1, sticky="nsew")
+button_equal.grid(row=4, column=2, sticky="nsew")
+button_divide.grid(row=4, column=3, sticky="nsew")
 
 root.mainloop()
