42b305817ef09036b0ba9bae68543e1a8666d196
Gaurund <pavel.makushev@gmail.com>
2025-08-13 15:30:31 +0300

    Entry

--- hello.py
+++ hello.py
@@ -3,10 +3,16 @@ from tkinter import ttk
 
 root = Tk()
 
-def myClick():
-    myLabel = ttk.Label(root, text="Look! I clicked a Button!!")
-    myLabel.grid(column=0,row=1)
+entry = ttk.Entry(root, width=50)
+entry.grid(column=0, row=0)
+entry.insert(0, "Enter your name...")
 
-myButton = ttk.Button(root, text="My Text", padding="10", command=myClick).grid(column=0, row=0)
+def myClick():
+    hello = "Hello " + entry.get().capitalize()
+    myLabel = ttk.Label(root, text=hello)
+    myLabel.grid(column=0,row=2)
+
+myButton = ttk.Button(root, text="Enter Your Name", padding="10", command=myClick)
+myButton.grid(column=0, row=1)
 
 root.mainloop()
