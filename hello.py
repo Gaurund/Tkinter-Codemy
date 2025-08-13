8e57824c75b14b15a35b655b979ae41b60628e8b
Gaurund <pavel.makushev@gmail.com>
2025-08-13 17:42:22 +0300

    Just

--- hello.py
+++ hello.py
@@ -3,10 +3,13 @@ from tkinter import ttk
 
 root = Tk()
 
+entry = ttk.Entry(root, width=50)
+entry.grid(column=0, row=0)
+
 def myClick():
     myLabel = ttk.Label(root, text="Look! I clicked a Button!!")
-    myLabel.grid(column=0,row=1)
+    myLabel.grid(column=0,row=3)
 
-myButton = ttk.Button(root, text="My Text", padding="10", command=myClick).grid(column=0, row=0)
+myButton = ttk.Button(root, text="My Text", padding="10", command=myClick).grid(column=0, row=1)
 
 root.mainloop()
