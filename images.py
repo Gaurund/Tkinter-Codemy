d394d3d0192ebad3b1e57e37007084c4f46a7bce
Gaurund <pavel.makushev@gmail.com>
2025-08-13 18:00:22 +0300

    Added Pillow

--- images.py
+++ images.py
@@ -1,9 +1,16 @@
-from tkinter import *
+from tkinter import Tk
 from tkinter import ttk
+from PIL import ImageTk, Image
 
 root = Tk()
 root.title("Learn to Code at Codemy.com")
-root.iconbitmap("")
+root.iconbitmap("tk.ico")
 
+my_img = ImageTk.PhotoImage(Image.open("D:/DOCS/PICS/2015-04-11/Сканировать1.JPG"))
+my_label = ttk.Label(root, image=my_img)
+my_label.grid()
+
+button_quit = ttk.Button(root, text="Exit", command=root.quit)
+button_quit.grid()
 
 root.mainloop()
